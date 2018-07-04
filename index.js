6f2ea4edc8c9c2e9fba3c1202087ea8f1fdc4242
okedo <okedo@mail.ru>
2018-07-04 23:58:43 +0300

    connect to db

--- index.js
+++ index.js
@@ -8,21 +8,27 @@ const mongoClient = require("mongodb").MongoClient;
 
 mongoClient.connect(
   "mongodb://localhost:27017/",
-  function(err, client) {
-    if (err) {
-      return console.log(err);
-    }
-    // взаимодействие с базой данных
-    client.close();
-  }
-);
+  function (err, client) {
+    const db = client.db("blogDb");
+    const collection = db.collection("articles");
+    let user = { name: "Tom", age: 23 };
+    collection.insertOne(user, function (err, result) {
+
+      if (err) {
+        return console.log(err);
+      }
+      console.log(result.ops);
+      client.close();
+    })
+  })
 
 var jsonParser = bodyParser.json();
 
-app.get("/main", function(request, response) {
-  response.send({});
+app.get("/main", function (request, response) {
+  console.log("ok");
+  response.send({"sdd":"sdad"});
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Сервер ожидает подключения...");
 });
