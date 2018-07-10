7b95d5910bf2837d585eea513a3dbc293c84c66c
Viktor Patraboi <vpatr@softserveinc.com>
2018-07-10 12:58:54 +0300

    register post updated

--- index.js
+++ index.js
@@ -12,75 +12,131 @@ const dbURI = "mongodb://admin:Update01@ds125381.mlab.com:25381/blogdb";
 // const dbURI = "mongodb://okedo:Update01@blogdb-shard-00-00-jifjv.mongodb.net:27017";
 // const dbURI = "mongodb+srv://okedo:<Update01>@blogdb-jifjv.mongodb.net/blogDb";
 
-app.get("/main", function (req, res) {
-
-  mongoClient.connect(dbURI, function (err, client) {
-    client.db("blogdb").collection("mainPage").find({}).toArray(function (err, mainPageData) {
-      res.send(mainPageData[0])
-      client.close();
-    });
-  });
-});
-
-app.get("/articles", function (request, response) {
+app.get("/main", function(req, res) {
   mongoClient.connect(
-    dbURI, { useNewUrlParser: true }, function (err, client) {
-      client.db("blogdb").collection("articles").find({}).toArray(function (err, data) {
-        response.send(data);
-        client.close();
-      });
-    });
+    dbURI,
+    function(err, client) {
+      client
+        .db("blogdb")
+        .collection("mainPage")
+        .find({})
+        .toArray(function(err, mainPageData) {
+          res.send(mainPageData[0]);
+          client.close();
+        });
+    }
+  );
 });
 
+app.get("/articles", function(request, response) {
+  mongoClient.connect(
+    dbURI,
+    { useNewUrlParser: true },
+    function(err, client) {
+      client
+        .db("blogdb")
+        .collection("articles")
+        .find({})
+        .toArray(function(err, data) {
+          response.send(data);
+          client.close();
+        });
+    }
+  );
+});
 
-app.post("/article/new", jsonParser, function (request, response) {
+app.post("/article/new", jsonParser, function(request, response) {
   if (!request.body) {
-    console.log('error');
+    console.log("error");
+    return response.sendStatus(400);
+  } else if (!request.body.title && !request.body.text) {
     return response.sendStatus(400);
   }
-  else if (!request.body.title && !request.body.text) {
-    return response.sendStatus(400);
-  };
-  const article = { title: request.body.title, text: request.body.text }
+  const article = { title: request.body.title, text: request.body.text };
 
   mongoClient.connect(
-    dbURI, { useNewUrlParser: true },
-    function (err, client) {
-      client.db("blogdb").collection("articles").insertOne(article, function (err, result) {
-        if (err) return response.status(400).send();
-        console.log(article);
-        response.send(JSON.stringify(article));
-        client.close();
-      });
-    });
+    dbURI,
+    { useNewUrlParser: true },
+    function(err, client) {
+      client
+        .db("blogdb")
+        .collection("articles")
+        .insertOne(article, function(err, result) {
+          if (err) return response.status(400).send();
+          console.log(article);
+          response.send(JSON.stringify(article));
+          client.close();
+        });
+    }
+  );
 });
 
-app.post("/logon", jsonParser, function (request, response) {
+app.post("/logon", jsonParser, function(request, response) {
   if (!request.body) {
-    console.log('error');
+    console.log("error");
+    return response.sendStatus(400);
+  } else if (!request.body.login && !request.body.password) {
     return response.sendStatus(400);
   }
-  else if (!request.body.login && !request.body.password) {
-    return response.sendStatus(400);
+  const credentials = {
+    login: request.body.login,
+    password: request.body.password
   };
-  const credentials = { login: request.body.login, password: request.body.password }
 
   mongoClient.connect(
-    dbURI, { useNewUrlParser: true },
-    function (err, client) {
-      client.db("blogdb").collection("users").find({login:credentials.login}, function (err, result) {
-        if (err) return response.status(400).send();
-        if (result.password == credentials.password) {
-          response.send(JSON.stringify(credentials));
-        }
-        else return response.status(401).send();
-        client.close();
-      });
-    });
+    dbURI,
+    { useNewUrlParser: true },
+    function(err, client) {
+      client
+        .db("blogdb")
+        .collection("users")
+        .find({ login: credentials.login }, function(err, result) {
+          if (err) return response.status(400).send();
+          if (result.password == credentials.password) {
+            response.send(JSON.stringify(credentials));
+          } else return response.status(401).send();
+          client.close();
+        });
+    }
+  );
 });
 
+app.post("/register", jsonParser, function(request, response) {
+  if (!request.body) {
+    console.log("error");
+    return response.sendStatus(400);
+  } else if (!request.body.login && !request.body.password) {
+    return response.sendStatus(400);
+  }
+  const credentials = {
+    login: request.body.login,
+    password: request.body.password
+  };
 
+  mongoClient.connect(
+    dbURI,
+    { useNewUrlParser: true },
+    function(err, client) {
+      client
+        .db("blogdb")
+        .collection("users")
+        .find({ login: credentials.login }, function(err, result) {
+          if (result) {
+            response.sendStatus(401);
+          } else {
+            client
+              .db("blogdb")
+              .collection("users")
+              .insertOne(credentials, function(err, result) {
+                response.send(JSON.parse(result));
+              });
+          }
+          client.close();
+        });
+    }
+  );
+});
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log("Сервер ожидает подключения...");
 });
